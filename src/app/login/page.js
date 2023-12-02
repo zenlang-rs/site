dba38a59f1c34ebb98f5bf2426fd8c2bd84f8d45
Mohit Raghav <mohit07raghav19@gmail.com>
2023-12-02 10:39:38 +0530

    feat: integrated signup,login and forgetpassword apis

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,12 +1,38 @@
 "use client";
 import React, { useState, useContext } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { ThemeContext } from "../../app/contextapi/ThemeContext";
 
 export default function Login() {
+  const router = useRouter();
+
   const { darkMode } = useContext(ThemeContext);
   const [passwordError, setPasswordError] = useState("");
+  const handleLoginIn = async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("pass").value;
+    const data = { email, password };
+    const host = process.env.SERVER_HOSTNAME;
+    console.log(data);
+    const response = await fetch(`${host}/api/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(data),
+    });
 
+    if (!response.ok) {
+      console.error("HTTP error", response.status);
+    } else {
+      const result = await response.json();
+      router.push("/");
+      console.log(result);
+    }
+  };
   const validatePassword = (password) => {
     const regex =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
@@ -60,20 +86,28 @@ export default function Login() {
             </div>
           </div>
           <form
-            onSubmit={(e) => e.preventDefault()}
+            onSubmit={(e) => handleLoginIn(e)}
             className="mt-8 space-y-9 ml-20px"
           >
             <div>
-              <label className="font-medium">Email</label>
+              <label htmlFor="email" className="font-medium">
+                Email
+              </label>
               <input
+                id="email"
+                name="email"
                 type="email"
                 required
                 className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
               />
             </div>
             <div>
-              <label className="font-medium">Password</label>
+              <label htmlFor="pass" className="font-medium">
+                Password
+              </label>
               <input
+                id="pass"
+                name="pass"
                 type="password"
                 required
                 onChange={handlePasswordChange}
